95bda3a54b289d5a25ff244fb8f801c29ad46bc2
Talha Akhtar <talhaakhtarkpr1@gmail.com>
2023-10-11 20:15:06 +0500

    added

---
+++ 29/src/app/servers/servers.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+  serverId:number=10;
+  serverStatus:string='offline'
+  serverCreated:string='No server was created';
+
+  getServerStatus(){
+    return this.serverStatus;
+  }
+
+
+  allowNewServer:boolean=false;
+
+constructor(){
+  setTimeout(() => {
+    this.allowNewServer=!this.allowNewServer;
+  }, 2000);
+}
+
+onCreateServer(){
+  this.serverCreated='Server was created'
+}
+
+}
